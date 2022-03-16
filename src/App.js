b2e6ece6c0fb85af6c662e7a2b4cca6fc50be910
rihacherni <rihabcherni000@gmail.com>
2022-03-16 14:24:28 +0100

    com 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GestionDechet from './tables/pages/GestionDechet'
+import GestionPoubelle from './tables/pages/GestionoPoubelle'
+import PageTransportDechet from './tables/pages/TransportDechet';
+import Pannes from './tables/pages/Pannes';
+import Users from './tables/pages/Users';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<div>
+	   <Router>
+			<Navbar />
+			<Routes>
+				<Route exact path='/utilisateurs' element={<Users/>}></Route>										
+				<Route exact path='/gestion-poubelle' element={<GestionPoubelle/>}></Route>				
+				<Route exact path='/transport-dechets' element={<PageTransportDechet/>}></Route>				
+			    <Route exact path='/gestion-dechets' element={<GestionDechet/>}></Route>
+				<Route exact path='/pannes' element={<Pannes/>}></Route>							
+			</Routes>
+	   </Router>
+		
+	</div>
+
+);
 }
 
 export default App;
